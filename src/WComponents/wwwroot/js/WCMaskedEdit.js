b0514433997c616d6d25a7edc7f98ca3053d7fd2
Kecskeméti Tamás <kecsket@gmail.com>
2020-11-24 21:11:13 +0100

    First steps, setcaretposition does not work

---
+++ src/WComponents/wwwroot/js/WCMaskedEdit.js
@@ -0,0 +1,20 @@
+﻿export function setCaretPosition(element, pos)
+{
+    alert('Vagyok');
+    //var ctrl = document.getElementById(inputId);
+
+    if (element.setSelectionRange)
+    {
+        element.focus();
+        element.setSelectionRange(pos, pos);
+
+    }
+    else if (ctrl.createTextRange)
+    {
+        var range = element.createTextRange();
+        range.collapse(true);
+        range.moveEnd('character', pos);
+        range.moveStart('character', pos);
+        range.select();
+    }
+}
